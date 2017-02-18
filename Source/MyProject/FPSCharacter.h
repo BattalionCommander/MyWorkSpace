80becdee6753619215566952aa6d9a4340c45180
BattalionCommander <835809508@qq.com>
2017-02-18 15:52:42 +0800

    添加人物模型

---
+++ Source/MyProject/FPSCharacter.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "FPSCharacter.generated.h"
+
+UCLASS()
+class MYPROJECT_API AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties设置该角色属性的默认值
+	AFPSCharacter();
+
+	// Called when the game starts or when spawned游戏开始时或生成时调用
+	virtual void BeginPlay() override;
+	
+	// Called every frame每帧调用
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input调用后将功能绑定到输入
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 处理前后移动的输入。
+	UFUNCTION()
+	void MoveForward(float Value);
+
+	// 处理左右移动的输入。
+	UFUNCTION()
+	void MoveRight(float Value);
+	
+	// 按下按键时设置跳跃标记。
+	UFUNCTION()
+	void StartJump();
+
+	// 松开按键时清除跳跃标记。
+	UFUNCTION()
+	void StopJump();
+
+	// FPS 摄像机。
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* FPSCameraComponent;
+
+	// 第一人称模型（手臂），仅对拥有玩家可见。
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* FPSMesh;
+};
